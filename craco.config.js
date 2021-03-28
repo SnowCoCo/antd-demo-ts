0c9cb9f54c6c149f8a86e192749cd9553b105129
SnowCoCo <1279393662@qq.com>
2021-03-28 23:19:21 +0800

    config craco.config.js : hot-reload

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,13 @@
 const CracoLessPlugin = require('craco-less');
 const path = require('path');
-const resolve = dir => path.resolve(__dirname, dir);
+const reactHotReloadPlugin = require('craco-plugin-react-hot-reload');//热加载
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry:[
+    entry: [
         './src/index.tsx'
     ],
-    reactScriptsVersion:"react-scripts",
+    reactScriptsVersion: "react-scripts",
     babel: {   //用来支持装饰器
         plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
     },
@@ -14,38 +15,44 @@ module.exports = {
         {
             plugin: CracoLessPlugin,
             options: {
-            lessLoaderOptions: {
-                lessOptions: {
-                modifyVars: { '@primary-color': '#1DA57A' },
-                javascriptEnabled: true,
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: { '@primary-color': '#1DA57A' },
+                        javascriptEnabled: true,
+                    },
                 },
             },
-            },
         },
-        // new UglifyJsPlugin({
-        //     uglifyOptions:{
-        //         compress:{
-        //             warnings:false,
-        //             drop_debugger:true,
-        //             drop_console:true
-        //         },
-        //     },
-        //     sourceMap:false,
-        //     parallel:true
-        // }),
+        {
+            plugin: reactHotReloadPlugin
+        }
     ],
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.scss$/,
-                loaders:['style-loader','css-loader']
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader']
             }
         ]
     },
-    resolve: {
+    webpack: {
         // 设置别名
         alias: {
-            '@': resolve('src')// 这样配置后 @ 可以指向 src 目录
-        }
+            '@': path.resolve("src"),// 这样配置后 @ 可以指向 src 目录
+            'react-dom': '@hot-loader/react-dom',
+        },
+        // plugins:[
+        //     new UglifyJsPlugin({
+        //         uglifyOptions: {
+        //             compress: {
+        //                 warnings: false,
+        //                 drop_debugger: true,
+        //                 drop_console: true,
+        //             },
+        //         },
+        //         sourceMap: false,
+        //         parallel: true,
+        //     }),
+        // ]
     }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.less';
-import Header from './components/header/Header';
+import Header from './components/Header';
+import { hot } from 'react-hot-loader'; //热加载
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div id="aside" style={{background:'#999'}}>aside</div>
+      <div id="main" style={{background:'#419',gridColumn:'2/span 2'}}>main</div>
+      <div id="footer"style={{background:'#740',gridColumn:'1/span 3'}}>footer</div>
     </div>
   );
 }
 
-export default App;
+export default hot(module)(App);
